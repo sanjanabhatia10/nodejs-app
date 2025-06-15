bc10316c6dbe3ab369de136baa736600ee902388
sanjanabhatia10 <125271084+sanjanabhatia10@users.noreply.github.com>
2025-06-15 20:27:43 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+
 module.exports = {
-  mongoURI: `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`,
+  mongoURI: process.env.MONGODB_URI || 
+    `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`,
   redisPort: process.env.REDIS_PORT,
   redisHost: process.env.REDIS_HOST,
   redisPassword: process.env.REDIS_PASSWORD
